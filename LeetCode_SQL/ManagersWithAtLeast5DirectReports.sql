6210ea0eec74a62ede655132d14c8b3051653302
Jocelyn Wan <wjt0728@gmail.com>
2025-05-30 14:46:40 -0700

    add: implement SQL queries for various data retrievals including article views, average processing time per machine, big countries, customers without transactions, employee bonuses, customer referees, invalid tweets, managers with reports, product sales analysis, recyclable and low-fat products, employee ID replacement, rising temperatures, and student examination attendance

---
+++ LeetCode_SQL/ManagersWithAtLeast5DirectReports.sql
@@ -0,0 +1,14 @@
+SELECT 
+    name 
+FROM 
+    Employee 
+WHERE 
+    id IN (
+        SELECT 
+            managerId 
+        FROM 
+            Employee 
+        GROUP BY 
+            managerId 
+        HAVING COUNT(*) >= 5
+    );
